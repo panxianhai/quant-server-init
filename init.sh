d5ff249182dec5416f0af432048287f8bf70ed48
panxianhai <panxianhai@gmail.com>
2021-07-07 10:28:00 +0800

    init.sh

---
+++ init.sh
@@ -0,0 +1,15 @@
+apt-get update && apt-get upgrade
+
+apt-get install curl vim wget unzip apt-transport-https lsb-release ca-certificates
+
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+
+sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+
+apt-get update
+
+apt-get install php7.4-fpm php7.4-mysql php7.4-curl php7.4-gd php7.4-mbstring php7.4-xml php7.4-xmlrpc php7.4-zip php7.4-opcache
+
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php/7.4/fpm/php.ini 
+
+systemctl restart php7.4-fpm  
